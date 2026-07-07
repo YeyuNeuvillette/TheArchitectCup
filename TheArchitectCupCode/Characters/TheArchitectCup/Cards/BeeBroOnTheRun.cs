fc0bad3fb2c012bef9f73d53219c42412a044218
YXL3 <476723463@QQ.com>
2026-07-07 15:12:41 +0800

    try-add-FindSomeoneToGetYou-and-BeeBroOnTheRun

--- TheArchitectCupCode/Characters/TheArchitectCup/Cards/BeeBroOnTheRun.cs
+++ TheArchitectCupCode/Characters/TheArchitectCup/Cards/BeeBroOnTheRun.cs
@@ -1,17 +1,37 @@
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.Powers;
 using STS2RitsuLib.Interop.AutoRegistration;
 
 namespace TheArchitectCup.Characters.TheArchitectCup.Cards;
 
-[RegisterCard(typeof(ArchitectCupCardPool))]
-public sealed class BeeBroOnTheRun() : ArchitectCupCard(1, CardType.Skill, CardRarity.Uncommon, TargetType.AllEnemies)
+[RegisterCard(typeof(ColorlessCardPool))]
+public sealed class BeeBroOnTheRun() : ArchitectCupCard(2, CardType.Power, CardRarity.Rare, TargetType.AllEnemies)
 {
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [
+        CardKeyword.Innate
+    ];
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new PowerVar<PersonalHivePower>(1m),
+        new PowerVar<SlowPower>(1m),
+    ];
+
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
+        foreach (Creature enemy in base.CombatState.HittableEnemies)
+        {
+            await PowerCmd.Apply<PersonalHivePower>(choiceContext, enemy, DynamicVars["PersonalHivePower"].BaseValue, base.Owner.Creature, this);
+            await PowerCmd.Apply<SlowPower>(choiceContext, enemy, DynamicVars["SlowPower"].BaseValue, base.Owner.Creature, this);
+        }
     }
 
     protected override void OnUpgrade()
     {
+        EnergyCost.UpgradeBy(-1);
     }
 }
--- TheArchitectCupCode/Characters/TheArchitectCup/Cards/FindSomeoneToGetYou.cs
+++ TheArchitectCupCode/Characters/TheArchitectCup/Cards/FindSomeoneToGetYou.cs
@@ -1,17 +1,34 @@
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Combat.History.Entries;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models.CardPools;
 using STS2RitsuLib.Interop.AutoRegistration;
+using TheArchitectCup.Characters.TheArchitectCup.Powers;
 
 namespace TheArchitectCup.Characters.TheArchitectCup.Cards;
 
-[RegisterCard(typeof(ArchitectCupCardPool))]
-public sealed class FindSomeoneToGetYou() : ArchitectCupCard(1, CardType.Skill, CardRarity.Rare, TargetType.Self)
+[RegisterCard(typeof(ColorlessCardPool))]
+public sealed class FindSomeoneToGetYou() : ArchitectCupCard(0, CardType.Skill, CardRarity.Common, TargetType.AnyAlly)
 {
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
+        if (cardPlay.Target == null || cardPlay.Target.Player == null)
+            return;
+        await PowerCmd.Apply<FindSomeoneToGetYouPower>(choiceContext, cardPlay.Target, 1m, base.Owner.Creature, this);
     }
 
-    protected override void OnUpgrade()
+    public override async Task BeforeHandDraw(Player player, PlayerChoiceContext choiceContext, ICombatState combatState)
     {
+        if (IsUpgraded && player == base.Owner && CombatManager.Instance.History.CardPlaysFinished.Any((CardPlayFinishedEntry e) => e.HappenedLastPlayerTurn(base.Owner) && e.CardPlay.Card == this))
+        {
+            CardPile? pile = base.Pile;
+            if (pile == null || pile.Type != PileType.Hand)
+            {
+                await CardPileCmd.Add(this, PileType.Hand);
+            }
+        }
     }
 }
